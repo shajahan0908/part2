9696cb4abcc3498d6f7731386a9cee9e54d614fe
shajahan0908 <74003062+shajahan0908@users.noreply.github.com>
2021-06-07 10:50:58 +0530

    Delete src directory

--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Header = ({ course }) => (course ? <h1>{course}</h1> : null);
-
-export default Header;
--- src/components/Total.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Total = ({ parts }) => {
-  let total = parts.reduce((a, b) => a + b.exercises, 0);
-  return <h3>Total of {total} exercises</h3>;
-};
-
-export default Total;
